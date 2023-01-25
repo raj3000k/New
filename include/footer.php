a47aa96e72606f2b969808976292b557da284079
raj3000k <rajmotwani38@gmail.com>
2023-01-25 13:59:10 +0530

    newww

---
+++ include/footer.php
@@ -0,0 +1,10 @@
+  </body>
+  <script src="js/jquery-3.4.1.min.js"></script>
+  <script src="js/toast.js"></script>
+  <script src="js/alert.js"></script>
+  <script src="js/materialize.min.js"></script>
+  <script type="text/javascript" src="js/datatables.min.js"></script>
+  <script type="text/javascript" src="js/socialcodia.js"></script>
+  <script type="text/javascript" src="js/chart.js"></script>
+  <script type="text/javascript" src="js/unanipharma.js"></script>
+</html>
